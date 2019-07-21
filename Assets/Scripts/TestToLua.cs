f47f92beae9b5705666dd5e168825815bd8524d5
linshuhe1 <695671725@qq.com>
2019-07-21 22:10:18 +0800

    add tolua framework(master 9d083f641f6a2cc528b807db44358e61fb4566f4)

---
+++ Assets/Scripts/TestToLua.cs
@@ -0,0 +1,26 @@
+﻿using LuaInterface;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestToLua : MonoBehaviour
+{
+    LuaState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = new LuaState();
+        state.Start();
+        state.DoFile("Main.lua");
+
+        LuaFunction func = state.GetFunction("Main");
+        func.Call();
+        func.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
